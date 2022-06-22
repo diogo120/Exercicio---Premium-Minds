d71767259db4624df892ab8815ee06670d3434a1
Diogo <d.lambelho@campus.fct.unl.pt>
2022-06-22 11:44:36 +0100

    Solução e testes

--- src/main/java/com/premiumminds/internship/screenlocking/ScreenLockinPattern.java
+++ src/main/java/com/premiumminds/internship/screenlocking/ScreenLockinPattern.java
@@ -1,5 +1,10 @@
 package com.premiumminds.internship.screenlocking;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
 /**
@@ -7,13 +12,60 @@ import java.util.concurrent.Future;
  */
 class ScreenLockinPattern implements IScreenLockinPattern {
 
+    private static final int POINTS = 9;
+
+    private boolean canConnect(int curr, int i, boolean[] visited){
+        if(Math.abs((i-1)/3 - (curr-1)/3)==1)
+            return true;
+        else if ((Math.abs((i-1)/3 - (curr-1)/3)==2 && Math.abs((i-1)%3 - (curr-1)%3)==1))
+            return true;
+        else if (((i-1)/3==(curr-1)/3 && Math.abs(i-curr)==1))
+            return true;
+        else if ((visited[(curr+i)/2]))
+            return true;
+
+        return false;
+    }
+
+    private int getAllPatterns(boolean[] visited, int curr, int length) {
+        if(length <= 1 )
+            return 1;
+
+        int paths = 0;
+
+        visited[curr] = true;
+
+        for(int i = 1; i<= POINTS; i++) {
+            if(!visited[i] && canConnect(curr,i, visited)) {
+                paths += getAllPatterns(visited.clone(), i, length-1);
+            }
+        }
+        return paths;
+    }
+
  /**
   * Method to count patterns from firstPoint with the length
   * @param firstPoint initial matrix position
   * @param length the number of points used in pattern
   * @return number of patterns
   */
-  public Future<Integer> countPatternsFrom(int firstPoint,int length) {
-    throw new RuntimeException("Not Implemented Yet");
-  };
+  public Future<Integer> countPatternsFrom(final int firstPoint, int length) {
+      //throw new RuntimeException("Not Implemented Yet");
+      if (length <= 0)
+          throw new RuntimeException("Length cannot be less than 1");
+
+      if (length >= 10)
+          throw new RuntimeException("Length cannot be more than 9");
+
+      if (firstPoint<=0 || firstPoint>=10)
+          throw new RuntimeException("Invalid point, must be between 1 and 9");
+
+      ExecutorService executor = Executors.newFixedThreadPool(4);
+
+      boolean[] visited = new boolean[POINTS+1];
+
+      Callable<Integer> getAllPatterns = () -> getAllPatterns(visited, firstPoint,length);
+
+      return executor.submit(getAllPatterns);
+  }
 }
--- src/test/java/com/premiumminds/internship/screenlocking/ScreenLockinPatternTest.java
+++ src/test/java/com/premiumminds/internship/screenlocking/ScreenLockinPatternTest.java
@@ -1,6 +1,7 @@
 package com.premiumminds.internship.screenlocking;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
 
 import java.util.Arrays;
 import java.util.concurrent.ExecutionException;
@@ -32,6 +33,70 @@ public class ScreenLockinPatternTest {
   public void ScreenLockinPatternTestFirst3Length2Test()  throws InterruptedException, ExecutionException, TimeoutException {
     Future<Integer> count  = new ScreenLockinPattern().countPatternsFrom(3, 2);
     Integer result = count.get(10, TimeUnit.SECONDS);
-    assertEquals(result.intValue(), 5);
+    assertEquals(5, result.intValue());
   }
+
+  @Test
+  public void ScreenLockinPatternTestFirst3Length5Test()  throws InterruptedException, ExecutionException, TimeoutException {
+    Future<Integer> count  = new ScreenLockinPattern().countPatternsFrom(3, 5);
+    Integer result = count.get(10, TimeUnit.SECONDS);
+    assertEquals(684, result.intValue());
+  }
+
+  @Test
+  public void ScreenLockinPatternTestFirst3Length8Test()  throws InterruptedException, ExecutionException, TimeoutException {
+    Future<Integer> count  = new ScreenLockinPattern().countPatternsFrom(3, 8);
+    Integer result = count.get(10, TimeUnit.SECONDS);
+    assertEquals(13792, result.intValue());
+  }
+
+  @Test
+  public void ScreenLockinPatternTestFirst3Length8EqualsFirst3Length9Test()  throws InterruptedException, ExecutionException, TimeoutException {
+    Future<Integer> count8  = new ScreenLockinPattern().countPatternsFrom(3, 8);
+    Integer result8 = count8.get(10, TimeUnit.SECONDS);
+    Future<Integer> count9  = new ScreenLockinPattern().countPatternsFrom(3, 9);
+    Integer result9 = count9.get(10, TimeUnit.SECONDS);
+    assertEquals(result8.intValue(), result9.intValue());
+  }
+
+  @Test
+  public void ScreenLockinPatternTestFirst3LengthNegativeTest()  throws InterruptedException, ExecutionException, TimeoutException {
+    Exception e = assertThrows(RuntimeException.class, ()->{
+      Future<Integer> count  = new ScreenLockinPattern().countPatternsFrom(3, -1);
+      Integer result = count.get(10, TimeUnit.SECONDS);
+    });
+    String message = "Length cannot be less than 1";
+    assertEquals(message, e.getMessage());
+  }
+
+  @Test
+  public void ScreenLockinPatternTestFirst3LengthLargerThan9Test()  throws InterruptedException, ExecutionException, TimeoutException {
+    Exception e = assertThrows(RuntimeException.class, ()->{
+      Future<Integer> count  = new ScreenLockinPattern().countPatternsFrom(3, 12);
+      Integer result = count.get(10, TimeUnit.SECONDS);
+    });
+    String message = "Length cannot be more than 9";
+    assertEquals(message, e.getMessage());
+  }
+
+  @Test
+  public void ScreenLockinPatternTestFirst11Length2Test()  throws InterruptedException, ExecutionException, TimeoutException {
+    Exception e = assertThrows(RuntimeException.class, ()->{
+      Future<Integer> count  = new ScreenLockinPattern().countPatternsFrom(11, 2);
+      Integer result = count.get(10, TimeUnit.SECONDS);
+    });
+    String message = "Invalid point, must be between 1 and 9";
+    assertEquals(message, e.getMessage());
+  }
+
+  @Test
+  public void ScreenLockinPatternTestFirst0Length2Test()  throws InterruptedException, ExecutionException, TimeoutException {
+    Exception e = assertThrows(RuntimeException.class, ()->{
+      Future<Integer> count  = new ScreenLockinPattern().countPatternsFrom(0, 2);
+      Integer result = count.get(10, TimeUnit.SECONDS);
+    });
+    String message = "Invalid point, must be between 1 and 9";
+    assertEquals(message, e.getMessage());
+  }
+
 }
